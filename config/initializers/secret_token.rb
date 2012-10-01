094d2003b69687ea25330d296e8e178bef25f508
Austin Gulati <contact@austingulati.com>
2012-09-30 22:49:02 -0400

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayWithMe::Application.config.secret_token = '361ea020d0c65111c30b136058925b7a34b987474bc3dba77043cb1de624f29dfce3a3f47a013c9de5f9189e802c580eb89afc601dc642714a32f49e1dd4bd75'
