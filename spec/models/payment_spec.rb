efcf548db7b70ce98cc7b997d4c37d461d5fbf61
Austin Gulati <contact@austingulati.com>
2012-10-30 15:30:38 -0400

    Add payment tests, for #36

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -16,5 +16,41 @@
 require 'spec_helper'
 
 describe Payment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @payer = FactoryGirl.create(:user)
+    @payee = FactoryGirl.create(:user)
+    @payment = Payment.new(amount: 5.95, payer: @payer, payee: @payee)
+  end
+
+  subject { @payment }
+  it { should be_valid }
+
+  describe "not valid" do
+    describe "when amount is not present" do
+      before { @payment.amount = "" }
+      it { should_not be_valid }
+    end
+
+    describe "when amount is not a number" do
+      before { @payment.amount = "cool" }
+      it { should_not be_valid }
+    end
+
+    describe "when amount is negative" do
+      before { @payment.amount = -5.75 }
+      it { should_not be_valid }
+    end
+
+    describe "when desired at is past" do
+      before { @payment.desired_at = Time.now - 86400 }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "valid" do
+    describe "when desired at is in the future" do
+      before { @payment.desired_at = Time.now + 86400 }
+      it { should be_valid }
+    end
+  end
 end
