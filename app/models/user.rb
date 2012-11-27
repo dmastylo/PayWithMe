553633fdc9d292c81193eecfb4949bbc3ac3c7e3
Austin Gulati <contact@austingulati.com>
2012-11-26 20:56:47 -0500

    Remove username field (Chris made a good suggestion!!!!)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
   def self.from_omniauth(auth)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127012538) do
+ActiveRecord::Schema.define(:version => 20121127015610) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(:version => 20121127012538) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.string   "username"
     t.string   "provider"
     t.string   "uid"
   end
