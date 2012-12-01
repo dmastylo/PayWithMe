56a1fdb0b41f205845c9a7b67ed861f471b7295c
Damian Mastylo <dmastylo@gmail.com>
2012-12-01 00:07:51 -0500

    Added validations for events and messages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :user_in_event, only: [:show]
   
   def new
     @event = current_user.organized_events.new
@@ -21,4 +22,16 @@ class EventsController < ApplicationController
     @members = @event.members
     @messages = @event.messages.all
   end
+
+  private
+      def user_in_event
+          @event = Event.find(params[:id])
+
+          if @event.members.include?(current_user) || @event.organizer == current_user
+              true
+          else
+              flash[:error] = "You're not on the list."
+              redirect_to root_path
+          end
+      end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
+    before_filter :user_in_event
+
     def create
-        @event = Event.find(params[:event_id])
+        # @event = Event.find(params[:event_id])
         @message = @event.messages.create(params[:message])
         @message.user = current_user
         if @message.save
@@ -11,4 +13,16 @@ class MessagesController < ApplicationController
             redirect_to event_path(@event)
         end
     end
+
+    private
+        def user_in_event
+            @event = Event.find(params[:event_id])
+
+            if @event.members.include?(current_user)
+                true
+            else
+                flash[:error] = "Trying to do something you're not supposed to...?"
+                redirect_to root_path
+            end
+        end
 end
