038f75517a34eb219c4de113a237b61cec15b5eb
celic <cceli@codequarry.net>
2012-12-06 22:43:49 -0500

    Adds tests for editing a user

--- spec/requests/account_settings_spec.rb
+++ spec/requests/account_settings_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 describe "Account Settings" do
 	
 	before do
+		
+		# Create a new user
 		@user = FactoryGirl.create(:user)
-		visit user_path
+		
+		# Login with that user
+		visit new_user_session_path
+		fill_in "Email", with: @user.email
+		fill_in "Password", with: "foobarbaz"
+		click_button "Sign in"
+
+		# Try to edit user information
+		visit edit_user_registration_path
 	end
 
 	subject{ page }
+
+	let(:submit){ "Update Info" }
 
 	describe "when profile picture" do
 
@@ -19,24 +31,49 @@ describe "Account Settings" do
 	end
 
 	describe "when name" do
-		describe "is changed" do
+		describe "is changed to something valid" do
+			before do
+				fill_in "user_name", with: "New Name"
+				click_button submit
+			end
 
+			it "should successfully save changes" do
+				response.should have_selector('div.alert', text: "Successfully Saved") #Needs to be fixed
+			end
 		end
 
-		#is blank
+		describe "is changed to be blank" do
+			before do
+				fill_in "user_name", with: ""
+				click_button submit
+			end
 
-		#exceeds maximum
+			it "should reject changes" do
+				response.should have_selector('div.alert-error') #Needs to be fixed
+			end
+		end
+
+		describe "is changed to exceed maximum" do
+			before do
+				fill_in "user_name", with: 'x'*60
+				click_button submit
+			end
+
+			it "should reject changes" do
+				response.should have_selector('div.alert-error') #Needs to be fixed
+			end
+		end
 	end
 
 	describe "when email" do
 		describe "is changed" do
 			before do
-				fill_in "email", with: "new_email@example.com"
+				fill_in "user_email", with: "new_email@example.com"
 				click_button "Update Info"
 			end
 
 			it "should successfully save changes" do
-				response.should have_selector('p', text: "Successfully Saved")
+				response.should have_selector('div.alert', text: "Successfully Saved") #Needs to be fixed
 			end
 		end
 
