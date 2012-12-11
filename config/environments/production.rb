73d937bef96c8c56999a89385ba886424560f500
Austin Gulati <contact@austingulati.com>
2012-12-10 21:20:26 -0500

    Precompile bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ PayWithMe::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.css.sass )
+  config.assets.precompile += %w( application.css.sass  bootstrap )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
