b04402c7552d3bce72ae60f77736d34d6cdd2aad
Austin Gulati <contact@austingulati.com>
2012-12-17 17:11:14 -0500

    Update default host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,7 @@ PayWithMe::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Default URL
+  routes.default_url_options[:host] = "paywme.herokuapp.com"
 end
