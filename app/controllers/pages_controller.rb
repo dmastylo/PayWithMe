744958fad8d8d06bcd953ff52b5dfd9d2a995449
Damian Mastylo <dmastylo@gmail.com>
2012-12-23 23:08:54 -0600

    Some basic output. Not working yet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
     def index
-        @upcoming_events = current_user.upcoming_events
+        @upcoming_events = current_user.upcoming_events if user_signed_in?
+        @news_items = current_user.news_items if user_signed_in?
     end
 end
