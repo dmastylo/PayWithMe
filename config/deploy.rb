21eee2924045b042cf9c83fc05de0bab1d0a1371
austingulati <contact@austingulati.com>
2012-12-26 19:58:12 -0500

    Load RVM PATH in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,14 @@
 require "bundler/capistrano"
 
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.    
+require "rvm/capistrano"
+
+set :rvm_ruby_string, 'ree@paywithme'
+set :rvm_type, :user  # Don't use system-wide RVM
+
 # server "198.61.183.12", :web, :app, :db, primary: true
 
 set :application, "PayWithMe"
