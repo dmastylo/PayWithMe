e342ef4f08f4b60edf88def68a92aff286bcb4e3
austingulati <contact@austingulati.com>
2013-01-05 15:34:35 -0500

    Clean up omniauth callbacks controller, fixes #104

--- app/controllers/my_devise/omniauth_callbacks_controller.rb
+++ app/controllers/my_devise/omniauth_callbacks_controller.rb
@@ -1,19 +1,37 @@
 class MyDevise::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
+    # Used for flash messages
+    provider = request.env["omniauth.auth"].provider.humanize
 
     if signed_in?
+      # Make sure account isn't in use
+      user = User.from_omniauth(request.env["omniauth.auth"])
+      if user.persisted? && user != current_user
+        flash[:error] = "That #{provider} account is already in use."
+        redirect_to edit_user_registration_path
+        return
+      end
+
+      # Set information if account is a stub
       if current_user.stub?
         current_user.name = request.env["omniauth.auth"].info.name if request.env["omniauth.auth"].info.name
         current_user.email = request.env["omniauth.auth"].info.email if request.env["omniauth.auth"].info.email
       end
+      
+      # Update user
       current_user.using_oauth = true
       current_user.save
       user = current_user
+      new_registration = false
     else
+      # Create new user
       user = User.from_omniauth(request.env["omniauth.auth"])
+      user.save
+      new_registration = true
     end
 
     if user.persisted?
+      # Update linked accounts
       linked_account = user.linked_accounts.find_by_provider(request.env["omniauth.auth"].provider)
       if linked_account.present?
         linked_account.uid = request.env["omniauth.auth"].uid
@@ -26,23 +44,35 @@ class MyDevise::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
 
       if signed_in?
         if current_user.stub?
+          # Final stub updates
           current_user.toggle(:stub)
           current_user.guest_token = nil
           current_user.save
-          flash[:success] = "Account registration successfully completed!"
+
+          # Stub flash message
+          flash[:success] = "Account registration successfully completed with #{provider} account!"
         else
           if update
-            flash[:success] = "Synced account successfully updated!"
+            # Account updated flash message
+            flash[:success] = "Synced #{provider} account successfully updated!"
           else
-            flash[:success] = "New account successfully synced!"
+            # Account added flash message
+            flash[:success] = "#{provider} account successfully synced!"
           end
           session["user_return_to"] = edit_user_registration_path
         end
       else
-        flash[:notice] = "Signed in successfully."
+        if new_registration
+          # Registration flash message
+          flash[:success] = "Registered with #{provider} successfully."
+        else
+          # Sign in flash message
+          flash[:notice] = "Signed in with #{provider} successfully."
+        end
       end
       sign_in_and_redirect user
     else
+      # Send things over to registration form
       session["devise.user_attributes"] = user.attributes
       session["devise.account_attributes"] = { provider: request.env["omniauth.auth"].provider, uid: request.env["omniauth.auth"].uid }
       redirect_to new_user_registration_path
