da21ec4377f41d5a2ee51f1b6bb3a38232d81ed7
celic <cceli@codequarry.net>
2013-01-17 14:42:20 -0500

    Fixes database again

--- db/migrate/20130117190518_remove_slug_from_events.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSlugFromEvents < ActiveRecord::Migration
-  def up
-    remove_column :events, :slug
-  end
-
-  def down
-    add_column :events, :slug, :string
-  end
-end
--- db/migrate/20130117190945_drop_friendly_id_table.rb
+++
@@ -1,9 +0,0 @@
-class DropFriendlyIdTable < ActiveRecord::Migration
-  def up
-  	drop_table :friendly_id_slugs
-  end
-
-  def down
-  	raise ActiveRecord::IrreversibleMigration
-  end
-end
