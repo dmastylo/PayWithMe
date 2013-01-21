d100bc9769ec57ded63f3d4f310973329d48592e
austingulati <contact@austingulati.com>
2013-01-21 15:35:15 -0500

    Fix up reset password function

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   config.mailer_sender = "noreply@paywith.me"
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = "Devise::Mailer"
+  config.mailer = "MyDevise::Mailer"
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
