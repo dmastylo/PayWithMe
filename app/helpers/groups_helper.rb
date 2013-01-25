8960a91552285e9b8e11c2b0f248a9eec2d98e80
celic <cceli@codequarry.net>
2013-01-24 23:20:07 -0500

    Keeps working with group images

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -5,4 +5,20 @@ module GroupsHelper
       title: group.title
     }
   end
+
+    def group_image_path(group, size=:thumb)
+    	if group.image.present?
+    		group.image.url(size)
+    	elsif group.image_url.present?
+    		group.image_url
+    	else
+    		asset_path("default_group_image.png")
+    	end
+    end
+
+    def group_image_tag(group, size=:thumb)
+    	width = Figaro.env.send(size.to_s+"_size")
+    	image = image_tag(group_image_path(group, size), width: width)
+    	link_to image, group
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124215937) do
+ActiveRecord::Schema.define(:version => 20130124225835) do
 
   create_table "contact_forms", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -103,10 +103,15 @@ ActiveRecord::Schema.define(:version => 20130124215937) do
   create_table "groups", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "slug"
     t.integer  "organizer_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "image_url"
   end
 
   add_index "groups", ["slug"], :name => "index_groups_on_slug"
