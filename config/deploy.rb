22720400643d6a53a9fd71f53f621858fed439c5
Austin Gulati <contact@austingulati.com>
2013-03-13 18:51:00 -0400

    Try new configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -96,6 +96,12 @@ namespace :deploy do
         Dir.glob(File.join("public", "assets", "*")).each do |file|
           directory.files.create(key: File.join("assets", File.basename(file)), body: File.open(file)) unless File.directory?(file)
         end
+        Dir.glob(File.join("public", "assets", "layout", "*")).each do |file|
+          directory.files.create(key: File.join("assets", "layout", File.basename(file)), body: File.open(file)) unless File.directory?(file)
+        end
+        Dir.glob(File.join("public", "assets", "icons", "*")).each do |file|
+          directory.files.create(key: File.join("assets", "icons", File.basename(file)), body: File.open(file)) unless File.directory?(file)
+        end
 
         logger.info "Removing local assets"
         run_locally "rm -rf public/assets"
