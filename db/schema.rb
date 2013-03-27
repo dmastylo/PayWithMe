b009c1935663657c5f237a688bb132bb72becdc2
celic <cceli@codequarry.net>
2013-03-27 16:28:43 -0400

    Prepares for name change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327144155) do
+ActiveRecord::Schema.define(:version => 20130327202550) do
 
   create_table "contact_forms", :force => true do |t|
     t.datetime "created_at", :null => false
