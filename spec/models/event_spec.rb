b58c2f3e29112dbc9d0a009e7936c203121ae18a
Austin Gulati <contact@austingulati.com>
2013-03-28 15:08:59 -0400

    Start fixing event tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -313,12 +313,14 @@ describe Event do
         @event.add_members FactoryGirl.create_list(:user, 10)
       end
 
-      # @Austin Gulati could you look at this?
-      # Off by like 6 bucks
       it "should have the correct split_amount_cents" do
-        receive_amount = ((@event.split_amount_cents - Figaro.env.fee_static.to_f * 100.0) * (1.0 - Figaro.env.fee_rate.to_f)).round / 100.0
-        receive_amount.should == amount
+        @event.split_amount.should == amount / @event.paying_member_count
       end
+
+      # it "should have the correct split_amount_cents" do
+        # receive_amount = ((@event.split_amount_cents - Figaro.env.fee_static.to_f * 100.0) * (1.0 - Figaro.env.fee_rate.to_f)).round / 100.0
+        # receive_amount.should == amount
+      # end
     end
   end
 
