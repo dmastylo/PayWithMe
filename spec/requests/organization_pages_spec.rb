43626f5590a33fb327ca435f2b2be7c9490a7081
celic <cceli@codequarry.net>
2013-03-30 17:39:10 -0400

    Adds more tests

---
+++ spec/requests/organization_pages_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+describe "Organizations page" do
+	before do
+		@organization = FactoryGirl.create(:organization)
+		visit organizations_path
+	end
+
+	subject { page }
+	let(:submit) { "Submit" }
+
+	describe "valid submission" do
+		before do
+			fill_in "Name", with: "New Testuser"
+			fill_in "Email", with: "example@email.com"
+			fill_in "Organization Name", with: "Sample Restaurant"
+		end
+
+		it "should add the organization to the database" do
+			expect{ click_button submit }.to change(Organization, :count).by(1)
+		end
+	end
+
+	describe "invalid submission" do
+
+		describe "with no name" do
+			before do
+				fill_in "Email", with: "example@email.com"
+				fill_in "Organization Name", with: "Sample Restaurant"
+			end
+
+			it "should not add the organization to the database" do
+				expect{ click_button submit }.not_to change(Organization, :count)
+			end
+		end
+
+		describe "with no email" do
+			before do
+				fill_in "Name", with: "New Testuser"
+				fill_in "Organization Name", with: "Sample Restaurant"
+			end
+
+			it "should not add the organization to the database" do
+				expect{ click_button submit }.not_to change(Organization, :count)
+			end
+		end
+
+		describe "with invalid email" do
+			before do
+				fill_in "Name", with: "New Testuser"
+				fill_in "Email", with: "test@example@email.com"
+				fill_in "Organization Name", with: "Sample Restaurant"
+			end
+
+			it "should not add the organization to the database" do
+				expect{ click_button submit }.not_to change(Organization, :count)
+			end
+		end
+
+		describe "with no organization name" do
+			before do
+				fill_in "Name", with: "New Testuser"
+				fill_in "Email", with: "example@email.com"
+			end
+
+			it "should not add the organization to the database" do
+				expect{ click_button submit }.not_to change(Organization, :count)
+			end
+		end
+
+		describe "with comment exceeding 250 characters" do
+			before do
+				fill_in "Name", with: "New Testuser"
+				fill_in "Email", with: "example@email.com"
+				fill_in "Organization Name", with: "Sample Restaurant"
+				fill_in "Comment", with: "a" * 251
+			end
+
+			it "should not add the organization to the database" do
+				expect{ click_button submit }.not_to change(Organization, :count)
+			end
+		end
+	end
+end
