98afb2a56939260fbc2dbc2025b00c01b570c27a
celic <cceli@codequarry.net>
2013-04-08 20:19:15 -0400

    Starts designing ticket

--- app/pdfs/ticket_pdf.rb
+++ app/pdfs/ticket_pdf.rb
@@ -2,7 +2,7 @@ class TicketPdf < Prawn::Document
 
 	# Initializes the document
 	def initialize(event, event_user)
-		super()
+		super(:page_layout => :landscape)
 		@event = event
 		@event_user = event_user
 
@@ -15,20 +15,22 @@ class TicketPdf < Prawn::Document
 
 		font("Lato")
 
-		info_table = make_table(fill_in_information, cell_style: { borders: [], overflow: :expand, inline_format: true })
+		info_table = make_table(fill_in_information, cell_style: { borders: [], overflow: :expand, inline_format: true, padding_left: 5, padding_right: 5 })
+		left_side = [[ info_table, {image: event_image, fit: [150, 150], vposition: :center, padding_left: 5, padding_top: 20 } ]]
+
 		ticket_table = make_table(fill_in_ticket, cell_style: { borders: [], overflow: :expand, inline_format: true })
 
-		full_data = [[info_table, ticket_table]]
+		full_data = [[left_side, ticket_table]]
+		table full_data
 
-		table full_data, cell_style: { borders: [] }
 		#generate_and_display_qr('http://www.paywith.me/paid', 200, 300)
 	end
 
 	# Fills in information, the left side of the ticket
 	def fill_in_information
 		[
-			["Your ticket to:"],
-			["#{@event.title}"],
+			["<font size='10'>Your ticket to:</font>"],
+			["<font size='24'><b>#{@event.title}</b></font>"],
 			["Name: #{@event_user.user.name || @event_user.user.email}"],
 			["Payment Method: #{@event_user.payments[0].payment_method.name}"],
 			["Date Paid: #{@event_user.paid_at}"],
@@ -44,33 +46,44 @@ class TicketPdf < Prawn::Document
 		]
 	end
 
-	# Displays image
+	# Displays qr code as grid
 	def generate_and_display_qr(link_to_embed, x_start, y_start)
 
 		@qr = RQRCode::QRCode.new(link_to_embed)
 
 		x_pos = x_start
 		y_pos = y_start
+		width = 5
 
 		@qr.modules.each_index do |x|
-
-		 	y_pos -= 5
-
+		 	y_pos -= width
 		 	@qr.modules.each_index do |y|
-
-				x_pos += 5
-
+				x_pos += width
 		    	if @qr.dark?(x,y)
 		    		fill_color "000000"
-		    		fill_rectangle [x_pos, y_pos], 5, 5
+		    		fill_rectangle [x_pos, y_pos], width, width
 		   		else
 		   			fill_color "FFFFFF"
-		   			fill_rectangle [x_pos ,y_pos], 5, 5
+		   			fill_rectangle [x_pos ,y_pos], width, width
 		   		end
 		   	end
-
 		   	x_pos = x_start
-
 		end
+	end
+
+	# Returns image path as string
+	def event_image
+	  if @event.image_type == :upload
+	    open(@event.image.url)
+	  elsif @event.image_type == :url
+	    open(@event.image_url)
+	  elsif @event.image_type == :default_image
+	    "#{Rails.root}/app/assets/images/default_event_image.png"
+	  end
+	end
+
+	# Return paywithme image path as string
+	def pwm_image
+		"#{Rails.root}/app/assets/images/logo_black.png"
 	end
 end
