e76b38c8d7dd10a80035a554e553e35b0dffcaf6
Austin Gulati <contact@austingulati.com>
2013-04-12 16:59:36 -0400

    Add WePay fees

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -124,6 +124,7 @@ class Payment < ActiveRecord::Base
         account_id: payee.wepay_account.token,
         amount: self.amount.to_s,
         app_fee: self.our_fee_amount.to_s,
+        fee_payer: "Payer",
         short_description: "Payment for #{self.event.title}",
         type: "EVENT",
         redirect_uri: Rails.application.routes.url_helpers.event_url(self.event, success: 1),
@@ -259,7 +260,7 @@ private
     end
 
     if self.payment_method.present? && self.amount_cents.present?
-      self.processor_fee_amount_cents = self.payment_method.processor_fee_after_our_fee(amount_cents)
+      self.processor_fee_amount_cents = self.payment_method.processor_fee(amount_cents)
       self.our_fee_amount_cents = self.payment_method.our_fee(amount_cents)
     end
   end
