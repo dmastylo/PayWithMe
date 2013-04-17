7ed6b84fdd521317cd6abdd24d2dcb6fd0d1c2b6
celic <cceli@codequarry.net>
2013-04-17 11:19:43 -0400

    Adds some error checking

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,13 @@
 class TicketsController < ApplicationController
 
 	def show
-		@event_user = EventUser.find_by_id(params[:event_user_id])
+
+		event_user_id = params[:event_user_id]
+
+		if(event_user_id.to_i < EventUser.count)
+			@event_user = EventUser.find_by_id(params[:event_user_id])
+		else
+			render 'errors/does_not_exist'
+		end
 	end
 end
