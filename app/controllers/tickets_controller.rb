f54ed204c4a7f819652fff91dff14477a709fb3c
celic <cceli@codequarry.net>
2013-04-17 11:43:21 -0400

    Adds security layer

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,7 +5,13 @@ class TicketsController < ApplicationController
 		event_user_id = params[:event_user_id]
 
 		if(event_user_id.to_i < EventUser.count)
-			@event_user = EventUser.find_by_id(params[:event_user_id])
+			@event_user = EventUser.find_by_id(event_user_id)
+			if(@event_user.event.organizer == current_user || @event_user.user == current_user)
+				render 'show'
+			else
+				flash[:error] = "You do not have permission to view that page. Only the event organizer and the payer can view the page. Please sign in if you are the organizer or payer."
+				redirect_to root_path
+			end
 		else
 			render 'errors/does_not_exist'
 		end
