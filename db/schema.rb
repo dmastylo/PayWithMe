c1277e1970f74635daf5f4b681c39cef16817a69
Damian Mastylo <dmastylo@codequarry.net>
2013-05-19 20:48:54 -0400

    Some minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(:version => 20130519073117) do
     t.integer  "paid_total_cents", :default => 0
     t.integer  "status",           :default => 0
     t.integer  "nudges_remaining", :default => 0
-    t.boolean  "accepted_invite"
   end
 
   add_index "event_users", ["event_id", "user_id"], :name => "index_event_users_on_event_id_and_user_id"
@@ -72,8 +71,8 @@ ActiveRecord::Schema.define(:version => 20130519073117) do
     t.string   "title"
     t.text     "description"
     t.datetime "due_at"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "division_type"
     t.integer  "total_amount_cents"
     t.integer  "split_amount_cents"
@@ -85,9 +84,6 @@ ActiveRecord::Schema.define(:version => 20130519073117) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "image_url"
-    t.integer  "fundraiser_goal_cents"
-    t.integer  "minimum_donation"
-    t.integer  "minimum_donation_cents"
     t.string   "guest_token"
   end
 
@@ -318,8 +314,8 @@ ActiveRecord::Schema.define(:version => 20130519073117) do
     t.datetime "completed_at"
     t.boolean  "admin"
     t.boolean  "send_emails",                :default => true
-    t.integer  "referrer_id"
     t.boolean  "using_cash",                 :default => false
+    t.integer  "referrer_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
